24f2530386782340eab00740a9aa366653347560
ahza64 <justinmichaelpettit@gmail.com>
2025-04-02 23:01:07 -0700

    build components IdeaForm, IdeaList and Modal

---
+++ client/src/components/IdeaList.js
@@ -0,0 +1,57 @@
+class IdeaList {
+  constructor() {
+    this._ideaListEl = document.querySelector('#idea-list');
+    this._ideas = [
+      {
+        text: 'This is a test idea',
+        username: 'testuser',
+        tag: 'technology',
+        id: 1,
+        date: '2023-10-01'
+      },
+      {
+        text: 'This is another test idea',
+        username: 'anotheruser',
+        tag: 'business',
+        id: 2,
+        date: '2023-10-02'
+      }
+    ];
+    this._validTags = new Set()
+    this._validTags.add('technology');
+    this._validTags.add('business');
+    this._validTags.add('health');
+    this._validTags.add('education');
+    this._validTags.add('software');
+    this._validTags.add('inventions');
+  }
+
+  getTagClass(tag) {
+    tag = tag.toLowerCase();
+    let tagClass = '';
+    if (this._validTags.has(tag)) {
+      tagClass = `tag-${tag}`;
+    } else {
+      tagClass = '';
+    }
+    return tagClass;
+  }
+
+  render() {
+    this._ideaListEl.innerHTML = this._ideas.map((idea) => {
+      const tagClass = this.getTagClass(idea.tag);
+      return `<div class="card">
+          <button class="delete"><i class="fas fa-times"></i></button>
+          <h3>
+            ${idea.text}
+          </h3>
+          <p class="tag ${tagClass}">${idea.tag.toUpperCase()}</p>
+          <p>
+            Posted on <span class="date">${idea.date}</span> by
+            <span class="author">${idea.username}</span>
+          </p>
+        </div>`}).join('');
+  }
+}
+
+export default IdeaList;
