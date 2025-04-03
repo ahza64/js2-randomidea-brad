56e0dcee04534766eef8fc9e2829d72f57f16691
ahza64 <justinmichaelpettit@gmail.com>
2025-04-03 15:31:03 -0700

    post and display idea. save user to local storage

--- client/src/components/IdeaForm.js
+++ client/src/components/IdeaForm.js
@@ -1,23 +1,34 @@
+import IdeasApi from "../services/ideasApi";
+import IdeaList from "./IdeaList";
+
 class IdeaForm {
   constructor () {
     this._formModal = document.querySelector('#form-modal');
+    this._ideaList = new IdeaList();
   }
 
   addEventListeners () {
     this._form.addEventListener('submit', this.handleSubmit.bind(this));
   }
 
-  handleSubmit (e) {
+  async handleSubmit (e) {
     e.preventDefault();
+    if (!this._form.elements.text.value || !this._form.elements.username.value || !this._form.elements.tag.value) {
+      alert('Please fill in all fields');
+      return;
+    }
+    localStorage.setItem('username', this._form.elements.username.value);
     const idea = {
       text: this._form.elements.text.value,
       username: this._form.elements.username.value,
       tag: this._form.elements.tag.value
     }
-    console.log(idea)
+    const newIdea = await IdeasApi.createIdea(idea);
+    this._ideaList.addIdeaToList(newIdea.data.data);
     this._form.elements.text.value = '';
     this._form.elements.username.value = '';
     this._form.elements.tag.value = '';
+    this.render()
 
     document.dispatchEvent(new Event('closemodal'));
   }
@@ -27,7 +38,7 @@ class IdeaForm {
       <form id="idea-form">
         <div class="form-control">
           <label for="idea-text">Enter a Username</label>
-          <input type="text" name="username" id="username" />
+          <input type="text" name="username" id="username" value="${localStorage.getItem('username') ? localStorage.getItem('username') : ''}" />
         </div>
         <div class="form-control">
           <label for="idea-text">What's Your Idea?</label>
